3708f13428afda021112f29546a80e1bca774d11
Ram <ramsundarstking927@gmail.com>
2025-09-07 16:05:42 +0530

    added basic features

---
+++ portfolio-web/src/components/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from '../sections/App.jsx'
+import "@fontsource/inter/300.css"; // Light
+import "@fontsource/inter/400.css"; // Regular
+import "@fontsource/inter/500.css"; // Medium
+import "@fontsource/inter/700.css"; // Bold
+import "@fontsource/inter/900.css"; // Black
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
